ad6b394ccdecf8f090a29f69c4e908de6508cfcf
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-08-29 14:50:37 +0100

    Second comit

---
+++ AppDelegate.h
@@ -0,0 +1,57 @@
+//
+//  AppDelegate.h
+//  PhysicsVideoStreameriPhone
+//
+//  Created by Joseph caxton-Idowu on 29/08/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FBConnect.h"
+
+@interface AppDelegate : NSObject <UIApplicationDelegate,NSXMLParserDelegate>{
+    
+    NSThread *SecondThread;
+    UIWindow *window;
+    UITabBarController *tabBarController;
+    NSString *SelectProductID;
+    UITableViewController *buyScreen;
+    NSString *DomainName;
+    NSMutableData *SubscriptionStatusData;
+    //NSMutableArray *SubscibedProducts;
+    BOOL PassageFlag;
+    BOOL EmailFlag;
+    NSString *UserEmail;
+    BOOL DoesUserHaveEmail;
+    BOOL AccessAll;
+    
+    // this is to handle return from facebook
+    
+    Facebook *m_facebook;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+@property (nonatomic, retain) NSThread *SecondThread;
+@property (nonatomic, retain) NSString *SelectProductID;
+@property (nonatomic, retain) UITableViewController *buyScreen;
+@property (nonatomic, retain) NSString *DomainName;
+@property (nonatomic, retain)  NSMutableData *SubscriptionStatusData;
+//@property (nonatomic, retain) NSMutableArray *SubscibedProducts;
+@property (nonatomic, assign) BOOL PassageFlag;
+@property (nonatomic, assign) BOOL EmailFlag;
+@property (nonatomic, retain) NSString *UserEmail;
+@property (nonatomic, assign) BOOL DoesUserHaveEmail;
+@property (nonatomic, assign) BOOL AccessAll;
+@property (nonatomic, retain)  Facebook *m_facebook;
+
+
+- (NSString *)applicationDocumentsDirectory;
+-(BOOL)isDeviceConnectedToInternet;
+- (BOOL)downloadFileIfUpdated:(NSString*)urlString:(NSString*)LocalFileLocation;
+- (NSString *)GetUUID;
+-(void)SubscriptionStatus:(NSString *)DeviceID;
+//-(void)WorkOutSubsriptionName:(NSMutableArray*)SubscibedProductsInArray;
+
+@end
