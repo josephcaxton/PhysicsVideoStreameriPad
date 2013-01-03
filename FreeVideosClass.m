465ccec7365e068f0320fd53eeed1ccd4f170aaa
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-01-03 15:19:54 +0000

    secure

--- FreeVideosClass.m
+++ FreeVideosClass.m
@@ -104,7 +104,7 @@
 	// Copy or Update the VideoConfig File;
    
     NSString *domain = appDelegate.DomainName;
-    NSString *queryFeed = [NSString stringWithFormat:@"%@/iosStream/Physics/PhysicsConfig.xml",domain]; 
+    NSString *queryFeed = [NSString stringWithFormat:@"%@/iosStreamv2/Physics/PhysicsConfig.xml",domain];
     NSString *Dir = [appDelegate.applicationDocumentsDirectory stringByAppendingPathComponent:@"PhysicsConfig.xml"];
     
          
@@ -252,7 +252,7 @@
     
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
     NSString *domain = appDelegate.DomainName;
-    NSString *queryFeed = [NSString stringWithFormat:@"%@/iosStream/Physics/PhysicsConfig.xml", domain];
+    NSString *queryFeed = [NSString stringWithFormat:@"%@/iosStreamv2/Physics/PhysicsConfig.xml", domain];
 	NSURLRequest *theRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:queryFeed]];
     NSURLResponse *resp = nil; 
     NSError *err = nil;
